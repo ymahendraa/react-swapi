dc248d8d060e1f69f509490438bb2505aaf70e99
ymahendraa <ymahendra721@gmail.com>
2023-12-10 07:13:56 +0800

    feat (page): index.html => add font feat (style): App.css => remove unused style feat (style): index.css => set default font feat (component): App.tsx => create Routing component feat (component): index.tsx => implement browser. router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// utils imports
+import { Routes, Route } from "react-router-dom"
 
-function App() {
+// components imports
+import Planet from "./pages/planet"
+import PlanetDetail from "./pages/planet/detail"
+import Layout from "./layout"
+import Wishlist from "./pages/wishlist"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Planet />} /> {/* Set this as the default route */}
+          <Route path="/planets" element={<Planet />} />
+          <Route path="/planets/:id" element={<PlanetDetail />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+        </Route>
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
